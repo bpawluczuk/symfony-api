1370a6c5f34790ef7054c79b73c86e48c2af8113
bpawluczuk <b.pawluczuk@companion.pl>
2018-09-14 08:28:35 +0200

    init

--- src/Controller/BpTestController.php
+++ src/Controller/BpTestController.php
@@ -12,6 +12,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * Class BpTestController
+ * @package App\Controller
+ */
 class BpTestController extends AbstractController
 {
     use ApiResponseObjects;
